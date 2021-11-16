6a9912cdd8c70adeb9040e220a0960124e40de86
Sasha Shevchenko <sasha.shevchenko.dk@gmail.com>
2021-11-16 16:17:19 +0100

    Added dotenv-rails and Cloudinary gems, Installed Active Storage by migrating , created backend logic for the new-costume-form

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -6,4 +6,23 @@ class CostumesController < ApplicationController
   def show
     @costume = Costume.find(params[:id])
   end
+
+  def new
+    @costume = Costume.new
+  end
+
+  def create
+    @costume = Costume.new(costume_params)
+    if @costume.save
+      # redirect_to user profile - but maybe index for now
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:name, :description, :size, :location, :category, :color, :price, :user_id, :occasion, :condition, :photo)
+  end
 end
--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,4 +1,11 @@
 class Costume < ApplicationRecord
+  SIZES = ["XS", "S", "M", "L", "XL", "XXL"]
+  CATEGORIES = ["Male", "Female", "Child", "Pets"]
+  COLORS = ["Black", "White", "Red", "Blue", "Yellow"]
+  OCCASIONS = ["Party", "Birthday", "Halloween", "Carnival", "Christmas"]
+  CONDITIONS = ["Very Used", "Gently used", "Good condition", "Very good condition", "Perfect condition"]
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
 end
