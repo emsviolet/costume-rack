3482c05b7eab9f36ef61baae2691f62cc7179708
pieroolivares <piero-olivares@hotmail.com>
2021-11-17 13:08:23 +0100

    added gems and updated map logic

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,9 +1,17 @@
 class CostumesController < ApplicationController
   def index
-    @costumes = Costume.all
+    @costumes = Costume.geocoded
+    @markers = @costumes.geocoded.map do |costume|
+      {
+        lat: costume.latitude,
+        lng: costume.longitude
+      }
+    end
   end
 
   def show
     @costume = Costume.find(params[:id])
   end
+
+
 end
