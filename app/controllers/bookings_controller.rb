94b0893d220706eae52ec82422cc07d1b356ed4a
Sasha Shevchenko <sasha.shevchenko.dk@gmail.com>
2021-11-18 17:04:33 +0100

    finished the create & new method in the bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,15 @@
 class BookingsController < ApplicationController
-
   def new
+    @costume = Costume.find(params[:costume_id])
     @booking = Booking.new
   end
 
   def create
+    @costume = Costume.find(params[:costume_id])
     @booking = Booking.new(booking_params)
+    @booking.costume = @costume
+    @booking.user = current_user
+
     if @booking.save!
       redirect_to root_path
     else
@@ -15,7 +19,7 @@ class BookingsController < ApplicationController
 
   private
 
-  def costume_params
+  def booking_params
     params.require(:booking).permit(:start_date, :end_date, :total_price, :user_id, :costume_id, :status, :message)
   end
 end
