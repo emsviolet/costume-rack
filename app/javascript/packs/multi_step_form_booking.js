c44a20488ee24ed4f95744df2d143eef38b4d0bb
pieroolivares <piero-olivares@hotmail.com>
2021-11-19 16:18:32 +0100

    nico was here

--- app/javascript/packs/multi_step_form_booking.js
+++ app/javascript/packs/multi_step_form_booking.js
@@ -4,39 +4,25 @@ const multiStepFormBooking = () => {
 
   const nextStepButton = document.querySelector(".next-step-booking");
   console.log(nextStepButton)
-  nextStepButton.addEventListener("click", (event) => {
-    event.preventDefault();
+  if (nextStepButton) {
+    nextStepButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      const currentStep = document.querySelector(`.${allSteps[counter]}`);
+      display(currentStep);
+      counter += 1;
 
-    // console.log(startDateMonthInput)
-    // const startDateYearInput = document.querySelector("#booking_start_date_1i").value;
-    // const startDateMonthInput = document.querySelector("#booking_start_date_2i").value;
-    // const startDateDateInput = document.querySelector("#booking_start_date_3i").value;
-
-    // const endDateYearInput = document.querySelector("#booking_end_date_1i").value;
-    // const endDateMonthInput = document.querySelector("#booking_end_date_2i").value;
-    // const endDateDateInput = document.querySelector("#booking_end_date_3i").value;
-
-    // const MessageInput = document.querySelector("#booking_message").value;
-
-    // console.log(startDateYearInput, startDateMonthInput, startDateDateInput);
-    // console.log(endDateYearInput, endDateMonthInput, endDateDateInput);
-    // console.log(MessageInput);
-
-    const currentStep = document.querySelector(`.${allSteps[counter]}`);
-    display(currentStep);
-    counter += 1;
-
-    if (counter === 3) {
-      const nextStep = document.querySelector(`.${allSteps[counter]}`);
-      display(nextStep)
-      display(nextStepButton)
-      const submitButton = document.querySelector(".submit")
-      display(submitButton)
-    } else {
-      const nextStep = document.querySelector(`.${allSteps[counter]}`);
-      display(nextStep)
-    };
-  });
+      if (counter === 3) {
+        const nextStep = document.querySelector(`.${allSteps[counter]}`);
+        display(nextStep)
+        display(nextStepButton)
+        const submitButton = document.querySelector(".submit")
+        display(submitButton)
+      } else {
+        const nextStep = document.querySelector(`.${allSteps[counter]}`);
+        display(nextStep)
+      };
+    });
+  }
 };
 
 function display(element) {
